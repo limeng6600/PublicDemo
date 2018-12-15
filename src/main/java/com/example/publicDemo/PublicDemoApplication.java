7c26b30afd645ad594c58bc520a5415ecbaaadc0
limeng6600 <34183273+limeng6600@users.noreply.github.com>
2018-12-15 15:36:00 +0800

    。

---
+++ src/main/java/com/example/publicDemo/PublicDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.publicDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicDemoApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/publicDemo/PublicDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.publicDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PublicDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
